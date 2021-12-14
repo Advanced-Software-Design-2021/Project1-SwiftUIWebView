3d16c0188bf565e6de340635afd190709ba9f697
Yuxuan Tian <danieltian.yx@gmail.com>
2021-12-14 14:04:57 -0500

    v4

--- TestBrowser/SwiftUIWebView/SwiftUIWebView/Model/DatabaseFunction.swift
+++ TestBrowser/SwiftUIWebView/SwiftUIWebView/Model/DatabaseFunction.swift
@@ -1,8 +1,8 @@
 //
-//  Databasefunction.swift
+//  DataBaseFunctions.swift
 //  SwiftUIWebView
 //
-//  Created by Daniel on 12/8/21.
+//  Created by Varnith Nookala on 12/8/21.
 //  Copyright © 2021 Md. Yamin. All rights reserved.
 //
 
@@ -14,40 +14,52 @@ public class DatabaseFunctions
 {
     let ref = Database.database().reference()
     var date: Date = Date()
-    //var databaseHandle = databaseHandle()?
+    
+    
     
     public func createTimestamp() -> String
     {
+        //initialize and format date
         self.date = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .long
         dateFormatter.locale = Locale(identifier: "en_US")
         dateFormatter.timeZone = TimeZone(abbreviation: "EST")
-        let dateString = dateFormatter.string(from: self.date)
-        return dateString
+        let dateString = dataFormatter.string(from: self.date)
         
+        return dateString
     }
+    
     public func writeToDataBase(key: String, dictionary: NSDictionary)
     {
-        //write to the database
+        //Write to the database
         self.ref.child(key).setValue(dictionary)
     }
     
     public func readFromDatabase()
     {
-        //connect
-        self.ref.observeSingleEvent(of: .value, with: {snapshot in
-            if !snapshot.exists(){return}
+        //Connect to database
+        self.ref.observeSingleEvent(of: .value, with: { snapshot in
+            
+            if !snapshot.exists() { return }
+            
             print(snapshot.value!)
+            
         })
     }
-    public func getHomepage(completion: @escaping(String)->())
+    
+    public func getHomepage(completion: @escaping (String) -> ())
     {
-        databaseHandle = ref.child("homepage").observe(.childAdded, with:{
-            (snapshot) in
+        databaseHandle = ref.child("homepage").observe(.childAdded, with:
+            { (snapshot) in
+            
             let value = snapshot.value as! String
+            
             completion(value)
-        })
+            
+            })
     }
 }
+
+
